c9718758fdd22b74e16d8eabb2638514f8c63426
송승우 <114336975+Dev-SSW@users.noreply.github.com>
2025-11-04 16:57:18 +0900

    Update 집합의 표현.java

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -5,56 +5,51 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static int[] parent;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        parent = new int[N + 1];
-
+        int M = Integer.parseInt(st.nextToken());   // M 개의 연산
+        parent = new int[N+1];  // n+1 개의 집합
         for (int i = 0; i <= N; i++) {
-            parent[i] = i;
+            parent[i] = i;      // 부모를 모두 자기 자신으로 초기화
         }
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int query = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken()); //0인지 1인지
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            if (query == 0) {
+            if (w==0) {
                 union(a, b);
-            } else {
-                if (checkSame(a, b)) {
+            }
+            else {
+                if (sameParent(a,b)) {
                     System.out.println("YES");
-                } else {
+                }
+                else {
                     System.out.println("NO");
                 }
             }
         }
     }
-
     static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
+        a = findParent(a);
+        b = findParent(b);
         if (a != b) {
             parent[b] = a;
         }
     }
-
-    static int find(int node) {
-        if (node == parent[node]) {
-            return node;
-        } else {
-            return parent[node] = find(parent[node]);
+    static int findParent(int n) {
+        if (n == parent[n]) {
+            return n;
+        }
+        else {
+            return parent[n] = findParent(parent[n]);
         }
     }
-
-    static boolean checkSame(int a, int b) {
-        a = find(a);
-        b = find(b);
-        return a == b;
+    static boolean sameParent(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+        return a==b;
     }
-
 }
