cc0fed63090bed2c940c55a72f4be71c44cd0ce3
송승우 <114336975+Dev-SSW@users.noreply.github.com>
2026-02-24 23:41:02 +0900

    [level 1] Title: 모의고사, Time: 0.93 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -8,35 +8,28 @@ class Solution {
         // answer의 크기 만큼 반복하여 1번, 2번, 3번 배열과 반복하고 점수 기록
         // 각각의 점수를 비교하여 가장 높은 점수를 받은 사람을 return
         // 같을 경우, 같은 경우에 대해 오름차순으로 return
-        int a,b,c;
-        a = b = c = 0;
+        int[] score = new int[3];
+        
         int[] f = new int[] {1, 2, 3, 4, 5};
         int[] s = new int[] {2, 1, 2, 3, 2, 4, 2, 5};
         int[] t = new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         for(int i = 0; i < answers.length; i++) {
             if (answers[i] == f[i%5]) {
-                a++;
+                score[0]++;
             }
             if(answers[i] == s[i%8]) {
-                b++;
+                score[1]++;
             }
             if(answers[i] == t[i%10]) {
-                c++;
+                score[2]++;
             }
         }
-        int[] sum = new int[]{a,b,c};
-        Arrays.sort(sum);
-        int max = sum[2];
+        
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
         
         List<Integer> answer = new ArrayList<>();
-        if (a == max) {
-            answer.add(1);
-        }
-        if (b == max) {
-            answer.add(2);
-        }
-        if (c == max) {
-            answer.add(3);
+        for (int p = 0; p < 3; p++) {
+            if (score[p] == max) answer.add(p + 1);
         }
         return answer;
     }
