b115279fc93abaf1c97108e49ca3e73a1ffb6d6b
송승우 <114336975+Dev-SSW@users.noreply.github.com>
2026-03-11 15:17:05 +0900

    Update 모음 사전.java

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -3,10 +3,9 @@ import java.util.*;
 class Solution {
     /** 
         A ~ UUUUU
-        A -> AA -> AAA -> AAAA -> AAAAA -> AAAAE -> AAAAI -> AAAAO -> AAAAU
-        -> AAAE -> AAAI -> AAAO -> AAAU -> AAE -> AAI -> AAO -> AAU -> AE
-        -> AI -> AO -> AU -> 
-        모든 조합을 구해야하니 dfs 활용
+        A - AA - AAA - AAAA - AAAAA - AAAAE - AAAAI - AAAAO - AAAAU - AAAE 
+        AAAEA - AAAEI - AAAEO - AAAEU - ...
+        조합을 구해야하니 dfs 활용
     */
     public char[] ch = {'A', 'E', 'I', 'O', 'U'};
     public int count = 0;
@@ -19,7 +18,7 @@ class Solution {
     public void dfs(String str, String word) {
         if (answer != 0) return;
         
-        // "" 도 포함할 수 있으니 조건문
+        // "" 도 포함할 수 있으니 조건문 (조건이 없으면 1 밀려남)
         if (!str.isEmpty()) {
             count++;
             if (str.equals(word)) {
