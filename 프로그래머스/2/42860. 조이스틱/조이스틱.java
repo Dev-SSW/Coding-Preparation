0797e0f92c0d566c6b5d3556e0c9041707f25e8a
송승우 <114336975+Dev-SSW@users.noreply.github.com>
2026-03-12 17:05:42 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,51 +1,53 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "JAN";
-        Solution solution = new Solution();
-        int result = solution.solution(name);
-        System.out.println(result);
-    }
-}
+import java.util.*;
+
 class Solution {
+    /**
+            글자 수에 따라 A, AA, AAA로 이루어진 String이 주어짐
+            1. 다음 알파벳으로 가는게 빠른지, 이전 알파벳으로 가는게 빠른지
+                - 모든 i에 대해 검사하여 결과에 더함
+            2. 어떤 방향으로 커서를 이동하는게 빠른지
+                - 오른쪽으로 몇 칸 갔다가 왼쪽?
+                - 왼쪽으로 몇 칸 갔다가 오른쪽?
+                - 아니면 순방향만 계속?
+                - 모든 i에 대해 검사하여 최소(cost)인 것 하나를 결과에 더함
+    */
+    public int findMin(char c) {
+        // dst - 'A' : 이후로 갔을 때
+        // ('Z' - 'A') - (dst - 'A') : 이전으로 갔을 때 (왼쪽 방향키 눌러야 하니 +1)
+        return Math.min(c - 'A', ('Z' - 'A' + 1) - (c - 'A'));
+    }
     public int solution(String name) {
         int answer = 0;
-        // 오른쪽으로 쭉 가는 경우 조이스틱을 움직인 수
         int cost = name.length() - 1;
         for (int i = 0; i < name.length(); i++) {
-            char dst = name.charAt(i);
+            char c = name.charAt(i);
             int count = 0;
-            if (dst != 'A') { //각 글자의 조이스틱 최소 움직임 수를 구하는 조건문
-                count = min(dst); //정방향으로 갔을 때와 역방향으로 갔을 때 중 최소인 것을 count에 저장
+            if (c != 'A') { 
+                // 문자 변경 비용 계산
+                count = findMin(c); 
             }
-            answer += count; //최종적으로 최소인 값을 answer에 더해준다. (다른 글자들도 만들어줘야하므로) A이면 count = 0이다.
-            // 왼쪽으로 돌아가는 경우 조이스틱을 움직인 수
-            int nextIndex = i + 1;
-
-            // 연속된 A가 끝난 인덱스가 nextIndex
-            while (nextIndex < name.length() && name.charAt(nextIndex) == 'A')
-                nextIndex++; //i+1부터 nextIndex가 'A'가 아닐 때까지  반복, (name의 길이를 벗어나지 않는 선에서)
-            // i * 2를 통해 0번 인덱스까지 돌아가는 횟수를 구한다.
-            // 그리고 name.length()에서 nextIndex를 빼 0에서 뒤로 가는데 필요한 횟수를 구한다.
-            cost = Math.min(Math.min(cost, (i * 2) + name.length() - nextIndex),(name.length() - nextIndex) * 2 + i);
-            //정방향으로 갔을 때의 cost와 비교
-            //i * 2의 이유, JBXAAAAAAB의 경우 현 위치가 만약 X일 때, JBX로 움직였다가 다시 XBJ로 돌아가므로, 왔다갔다를 해서
-            //*2를 해주는 것이다. 그 다음에 name.length()에서 nextIndex를 빼면 오른쪽 마지막 문자열부터 nextIndex까지의
-            //거리를 구할 수 있다.
-            //결론, 반복문을 통해서 각 문자열로 가는 최소를 min함수로 구하고, 왼쪽으로 갈지 오른쪽으로 갈지에 대한 최소를
-            //while문과 Math.min을 통해 구한 후 두 최소값을 answer에 더해준다.
-            //각 문자열의 최소는 바로 더해줘도 상관 없지만 조이스틱 왼쪽 오른쪽은 가장 최소인 것을 더해줘야하므로 나중에 더해준다.
-            //생각해볼 테스트 케이스 AAAAAABB, BABAAAAABB, BBBBBBAA 세 개정도
-            //for문을 계속 돌며 현재 인덱스에서 좌우 조이스틱을 어디로 움직여야 최소인지를 계속해서 탐색한다.
+            // for i를 통해 각 문자 별 변경 비용을 answer에 더해줍니다
+            answer += count;  
+            // 다음 인덱스부터 A가 얼마나 길게 있는지 확인
+            int nextIndex = i + 1;        
+            while (nextIndex < name.length() && 
+                   name.charAt(nextIndex) == 'A') {
+                nextIndex++;                 
+            }
+            // 커서 이동 비용 계산 (어디를 먼저 처리하는게 좋을지 계산)
+            // cost : 커서를 정방향 순서대로만 갔을 때 비용
+            // (i * 2) + name.length() - nextIndex : 
+            // - 시작점부터 현재 자리까지 갔다가, 다시 시작점으로 되돌아옴
+            // - 역방향으로 A를 건너뜀
+            // (name.length() - nextIndex) * 2 + i :
+            // - 시작점부터 역방향으로 A를 건너뛰고, 다시 끝으로 되돌아옴
+            // - 시작점으로부터 현재 자리까지 감
+            cost = 
+                Math.min(Math.min(cost, (i * 2) + name.length() - nextIndex),
+                (name.length() - nextIndex) * 2 + i);
         }
         answer += cost;
         return answer;
-    }
-
-    public int min(char dst) {
-        return Math.min(dst - 'A', ('Z' - 'A' + 1) - (dst - 'A'));
-        //JAZ라고 했을 때, J에서 A까지의 거리가 더 빠른지, 아니면 반대로 갔을 때가 빠른지를 확인하는 함수
-        //Z에서 A까지의 거리 + 1 에서 J에서 A까지의 거리를 빼면 Z에서 J까지의 거리가 나온다.
-        //한번 더 가야지 J가 적용되므로 + 1를 한 것이다.
     }
 }
 
