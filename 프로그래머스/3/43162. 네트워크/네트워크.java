e0796410618170efc497984a189690dd1a0f9daf
송승우 <114336975+Dev-SSW@users.noreply.github.com>
2026-07-26 10:45:40 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    static boolean[] visited;
+    // 조합 -> DFS
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for (int i = 0; i < n ; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i, computers);
+            }
+        }
+        return answer;
+    }
+    
+    public void dfs(int x, int[][] arr) {
+        visited[x] = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i] && arr[x][i] == 1) {
+                dfs(i, arr);
+            }
+        }
+    }
+}
